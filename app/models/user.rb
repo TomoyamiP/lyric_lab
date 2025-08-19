6662114851d3364a56e3db4b245ce77c5d9900e6
Naing Oo Lwin <naingoolwin@gmail.com>
2025-08-19 14:28:37 +0900

    added sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ApplicationRecord
 
   has_many :songs, dependent: :destroy
 
+  MUSIC_STYLES = ["Pop", "Rock", "Country", "Blues", "Hip-Hop", "Jazz", "World", "Alternative", "R&B", "Folk", "Reggae", "Gospel", "Metal", "Punk"]
+  MUSICIAN_TYPES = ["Beginner", "Hobbyist", "Semi-Pro", "Professional", "Virtuoso"]
+
   validates :username, presence: true, uniqueness: true
-  validates :music_style, presence: true, inclusion: { in: ["Pop", "Rock", "Country", "Blues", "Hip-Hop", "Jazz", "World", "Alternative", "R&B", "Folk", "Reggae", "Gospel", "Metal", "Punk"] }
-  validates :musician_type, presence: true, inclusion: { in: ["Beginner", "Hobbyist", "Semi-Pro", "Professional", "Virtuoso"] }
+  validates :music_style, presence: true, inclusion: { in: MUSIC_STYLES }
+  validates :musician_type, presence: true, inclusion: { in: MUSICIAN_TYPES }
   validates :age, presence: true
   validates :gender, presence: true
 end
